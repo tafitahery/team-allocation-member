f05bf8fa0b2ee70187b9996b2c6e0dd33f31c53f
Tafita Hery <tafitahery@gmail.com>
2023-01-07 15:20:45 +0300

    Understanding Props

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,14 @@
-const Header = () => {
+const Header = ({ selectedTeam, teamMemberCount }) => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team member allocation</h1>
+          <h3>
+            {selectedTeam} has {teamMemberCount} members
+          </h3>
+        </div>
+      </div>
     </header>
   );
 };
