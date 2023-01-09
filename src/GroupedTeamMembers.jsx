e97963b21ee78061873c76f9f2cb68caf89a2a08
Tafita Hery <tafitahery@gmail.com>
2023-01-09 17:27:44 +0300

    Create Employee Component

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -1,11 +1,96 @@
-const GroupedTeamMembers = () => {
+import { useState } from 'react';
+
+const GroupedTeamMembers = ({ employees, selectedTeam, setTeam }) => {
+  const groupTeamMembers = () => {
+    let teams = [];
+
+    const teamAMembers = employees.filter(
+      (employee) => employee.teamName === 'TeamA'
+    );
+    const teamA = {
+      team: 'TeamA',
+      members: teamAMembers,
+      collapsed: selectedTeam !== 'TeamA',
+    };
+    teams.push(teamA);
+
+    const teamBMembers = employees.filter(
+      (employee) => employee.teamName === 'TeamB'
+    );
+    const teamB = {
+      team: 'TeamB',
+      members: teamBMembers,
+      collapsed: selectedTeam !== 'TeamB',
+    };
+    teams.push(teamB);
+
+    const teamCMembers = employees.filter(
+      (employee) => employee.teamName === 'TeamC'
+    );
+    const teamC = {
+      team: 'TeamC',
+      members: teamCMembers,
+      collapsed: selectedTeam !== 'TeamC',
+    };
+    teams.push(teamC);
+
+    const teamDMembers = employees.filter(
+      (employee) => employee.teamName === 'TeamD'
+    );
+    const teamD = {
+      team: 'TeamD',
+      members: teamDMembers,
+      collapsed: selectedTeam !== 'TeamD',
+    };
+    teams.push(teamD);
+
+    return teams;
+  };
+  const [groupedEmployee, setGroupData] = useState(groupTeamMembers());
+
+  const handleTeamClick = (event) => {
+    const transformedGroupData = groupedEmployee.map((groupedData) =>
+      groupedData.team === event.currentTarget.id
+        ? { ...groupedData, collapsed: !groupedData.collapsed }
+        : groupedData
+    );
+    setGroupData(transformedGroupData);
+    setTeam(event.currentTarget.id);
+  };
+
   return (
     <main className="container">
-      <div className="row justify-content-center mt-3 mb-4">
-        <div className="col-8">
-          <h1>Grouped Team Members</h1>
+      {groupedEmployee.map((item) => (
+        <div
+          key={item.team}
+          className="card mt-2"
+          style={{ cursor: 'pointer' }}
+        >
+          <h4
+            id={item.team}
+            className="card-header text-secondary bg-white"
+            onClick={handleTeamClick}
+          >
+            Team name: {item.team}
+          </h4>
+          <div
+            id={'collapse_' + item.team}
+            className={item.collapsed ? 'collapse' : ''}
+          >
+            <hr />
+            {item.members.map((member) => (
+              <div key={member.id} className="mt-2">
+                <h5 className="card-title mt-2">
+                  <span className="text-dark">
+                    Full Name: {member.fullName}
+                  </span>
+                </h5>
+                <p>Designation: {member.designation}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ))}
     </main>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,8 @@ const Header = ({ selectedTeam, teamMemberCount }) => {
         <div className="col-8">
           <h1>Team member allocation</h1>
           <h3>
-            {selectedTeam} has {teamMemberCount} members
+            {selectedTeam} has {teamMemberCount}{' '}
+            {teamMemberCount > 1 ? 'Members' : 'Member'}
           </h3>
         </div>
       </div>
